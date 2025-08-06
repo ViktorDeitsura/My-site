4a3f125946079955b1192b583459b0010a07a6db
Viktor Deitsura <ViktorDeitsura@gmail.com>
2025-08-06 20:33:06 +0700

    footer v1.0

---
+++ src/widgets/footer/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="flex justify-center border border-solid">
+                <div className="container flex justify-between">
+                    <div>LOGO</div>
+                    <div>CONTENT</div>
+                    <div>SOCIAL</div>
+                </div>
+            </footer>  
+        </> 
+    );
+}
